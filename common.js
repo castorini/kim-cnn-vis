442fbc5976ec8e8b72e2b5033d26a114bb5580fd
Michael Tu <tuzhucheng@outlook.com>
2018-06-03 11:26:59 -0500

    Move inference heatmap to Inference tab

--- common.js
+++ common.js
@@ -43,28 +43,31 @@ function setup() {
    });
 }
 
+var SearchBar = {};
 $(document).ready(function() {
-  var sampleSentences = sampleInputs.map(d => d.sentence);
-  var input = document.getElementById("query");
-  var comboplete = new Awesomplete(input, {
-    minChars: 0,
-    maxItems: 12,
-  	list: sampleSentences,
-    sort: function (a, b) {
-      return a.length > b.length;
-    }
-  });
+  SearchBar.initAutocomplete = function() {
+    var sampleSentences = sampleInputs.map(d => d.sentence);
+    var input = document.getElementById("query");
+    var comboplete = new Awesomplete(input, {
+      minChars: 0,
+      maxItems: 12,
+      list: sampleSentences,
+      sort: function (a, b) {
+        return a.length > b.length;
+      }
+    });
 
-  Awesomplete.$('#query').addEventListener("click", function() {
-  	if (comboplete.ul.childNodes.length === 0) {
-  		comboplete.minChars = 0;
-  		comboplete.evaluate();
-  	}
-  	else if (comboplete.ul.hasAttribute('hidden')) {
-  		comboplete.open();
-  	}
-  	else {
-  		comboplete.close();
-  	}
-  });
+    Awesomplete.$('#query').addEventListener("click", function() {
+    	if (comboplete.ul.childNodes.length === 0) {
+    		comboplete.minChars = 0;
+    		comboplete.evaluate();
+    	}
+    	else if (comboplete.ul.hasAttribute('hidden')) {
+    		comboplete.open();
+    	}
+    	else {
+    		comboplete.close();
+    	}
+    });
+  }
 });
