513f3c587f040d6aea944f1b51e0bb9bb89d481c
Michael Tu <tuzhucheng@outlook.com>
2018-08-05 16:30:03 -0400

    Remove model weight/dev global vars

--- common.js
+++ common.js
@@ -1,13 +1,6 @@
-var weights;
-var bias;
-var fc_weights;
-var fc_bias;
-var dev_sentences;
-var dev_labels;
-
-function initializeParameters() {
+function initializeParameters(cb) {
   setTimeout(function(){
-    setup();
+    setup(cb);
   }, 200);
 }
 
@@ -15,45 +8,31 @@ function updateMsg(msg) {
   document.getElementById('message').innerHTML = "<div class='alert alert-success alert-dismissible' role='alert'><button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button>" + msg + "</div>";
 }
 
-function setup() {
+function setup(cb) {
   var startTime = window.performance.now();
+  var modelData = {};
   updateMsg("Started loading parameters.");
 
-  searcher.getBias("bias_3", function(bias_3) {
-     searcher.getBias("bias_4", function(bias_4) {
-       searcher.getBias("bias_5", function(bias_5) {
+  searcher.getParams(function(params) {
+    modelData.weights = Array(params.conv3_weights, params.conv4_weights, params.conv5_weights);
+    modelData.bias = Array(params.conv3_biases, params.conv4_biases, params.conv5_biases);
+    modelData.fc_weights = params.fc_weights;
+    modelData.fc_bias = params.fc_biases;
 
-         searcher.getWeights("weights_3", function(weights_3) {
-           searcher.getWeights("weights_4", function(weights_4) {
-             searcher.getWeights("weights_5", function(weights_5) {
-
-                searcher.getBias("bias_fc1", function(bias_fc1) {
-                  searcher.getWeights("weights_fc1", function(weights_fc1) {
-                    weights = Array(weights_3, weights_4, weights_5);
-                    bias = Array(bias_3, bias_4, bias_5);
-                    fc_weights = weights_fc1;
-                    fc_bias = bias_fc1;
-
-                    searcher.getSentenceAll(function(sentences, labels) {
-                      dev_sentences = sentences;
-                      dev_labels = labels;
-                      updateMsg("Finished loading parameters in " + (window.performance.now() - startTime)/1000 + "s.");
-                    });
-                  });
-                });
-              });
-            });
-          });
-       });
-     });
-   });
+    searcher.getSentenceAll(function(sentences, labels) {
+      modelData.dev_sentences = sentences;
+      modelData.dev_labels = labels;
+      updateMsg("Finished loading parameters in " + (window.performance.now() - startTime)/1000 + "s.");
+      cb(modelData);
+    });
+  });
 }
 
 var SearchBar = {};
 $(document).ready(function() {
   SearchBar.initAutocomplete = function() {
     var sampleSentences = sampleInputs.map(d => d.sentence);
-    var input = document.getElementById("query");
+    var input = document.getElementById('query');
     var comboplete = new Awesomplete(input, {
       minChars: 0,
       maxItems: 12,
@@ -63,17 +42,17 @@ $(document).ready(function() {
       }
     });
 
-    Awesomplete.$('#query').addEventListener("click", function() {
-    	if (comboplete.ul.childNodes.length === 0) {
-    		comboplete.minChars = 0;
-    		comboplete.evaluate();
-    	}
-    	else if (comboplete.ul.hasAttribute('hidden')) {
-    		comboplete.open();
-    	}
-    	else {
-    		comboplete.close();
-    	}
+    Awesomplete.$('#query').addEventListener('click', function() {
+      if (comboplete.ul.childNodes.length === 0) {
+        comboplete.minChars = 0;
+        comboplete.evaluate();
+      }
+      else if (comboplete.ul.hasAttribute('hidden')) {
+        comboplete.open();
+      }
+      else {
+        comboplete.close();
+      }
     });
   }
 });
--- wordvec-server/index.js
+++ wordvec-server/index.js
@@ -1,8 +1,7 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var word2vec = require('../word2vec');
-var word2veclarge = require('../word2vec_large');
+var word2vec = require('../word2vec_large');
 
 app.get('/', function(req, res){
   res.send('<h1>Hello world</h1>');
@@ -11,17 +10,10 @@ app.get('/', function(req, res){
 io.set('origins', '*:*');
 io.on('connection', function(socket){
   socket.on('wordvecs', function(msg){
-    console.log('Preparing to send 50d word vectors: ' + msg);
+    console.log('Preparing to send 300d word vectors: ' + msg);
 
     io.emit('length', {collection: 'wordvecs', length: word2vec.word2vec.length});
     word2vec.word2vec.forEach(d => io.emit('wordvecs', d));
-  });
-
-  socket.on('wordvecslarge', function(msg){
-    console.log('Preparing to send 300d word vectors: ' + msg);
-
-    io.emit('length', {collection: 'wordvecslarge', length: word2veclarge.word2vec.length});
-    word2veclarge.word2vec.forEach(d => io.emit('wordvecslarge', d));
   });
 });
 
