ec60ddf8605e92671213c0095b38d56cc14861cc
Michael Tu <tuzhucheng@outlook.com>
2018-09-09 23:01:11 -0600

    Replace tabs with spaces

--- js/matrix.js
+++ js/matrix.js
@@ -27,125 +27,125 @@ OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
 function Matrix(data, options) {
-	var words = ["'s", "exceptional", "performances"];
-	var margin = options.margin,
-	    width = options.width,
-	    height = options.height,
-	    container = options.container,
-	    showLabels = options.show_labels,
-	    startColor = options.start_color,
-	    endColor = options.end_color,
+  var words = ["'s", "exceptional", "performances"];
+  var margin = options.margin,
+      width = options.width,
+      height = options.height,
+      container = options.container,
+      showLabels = options.show_labels,
+      startColor = options.start_color,
+      endColor = options.end_color,
       highlightCellOnHover = options.highlight_cell_on_hover,
       highlightCellColor = options.highlight_cell_color;
 
-	var dataValues = data['values'];
-	var dataLabels = data['labels'];
+  var dataValues = data['values'];
+  var dataLabels = data['labels'];
 
-	if (!dataValues) {
-		throw new Error('data is empty');
-	}
+  if (!dataValues) {
+    throw new Error('data is empty');
+  }
 
-	if (!Array.isArray(dataValues) || !dataValues.length || !Array.isArray(dataValues[0])) {
-		throw new Error('2-D array expected');
-	}
+  if (!Array.isArray(dataValues) || !dataValues.length || !Array.isArray(dataValues[0])) {
+    throw new Error('2-D array expected');
+  }
 
   var maxValue = d3.max(dataValues, function(layer) { return d3.max(layer, function(d) { return d; }); });
   var minValue = d3.min(dataValues, function(layer) { return d3.min(layer, function(d) { return d; }); });
 
-	var numrows = dataValues.length;
-	var numcols = dataValues[0].length;
+  var numrows = dataValues.length;
+  var numcols = dataValues[0].length;
 
-	var svg = d3.select(container).append("svg")
-		.attr("width", width + margin.left + margin.right)
-		.attr("height", height + margin.top + margin.bottom)
-		.append("g")
-		.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+  var svg = d3.select(container).append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-	var background = svg.append("rect")
-		.attr("width", width)
-		.attr("height", height);
+  var background = svg.append("rect")
+    .attr("width", width)
+    .attr("height", height);
 
-	var x = d3.scale.ordinal()
-		.domain(d3.range(numcols))
-		.rangeBands([0, width]);
+  var x = d3.scale.ordinal()
+    .domain(d3.range(numcols))
+    .rangeBands([0, width]);
 
-	var y = d3.scale.ordinal()
-		.domain(d3.range(numrows))
-		.rangeBands([0, height]);
+  var y = d3.scale.ordinal()
+    .domain(d3.range(numrows))
+    .rangeBands([0, height]);
 
-	var colorMap = d3.scale.linear()
-		.domain([minValue,maxValue])
-		.range([startColor, endColor]);
+  var colorMap = d3.scale.linear()
+    .domain([minValue,maxValue])
+    .range([startColor, endColor]);
 
-	var row = svg.selectAll(".row")
-		.data(dataValues)
-		.enter().append("g")
-		.attr("class", "row")
-		.attr("transform", function(d, i) { return "translate(0," + y(i) + ")"; });
+  var row = svg.selectAll(".row")
+    .data(dataValues)
+    .enter().append("g")
+    .attr("class", "row")
+    .attr("transform", function(d, i) { return "translate(0," + y(i) + ")"; });
 
-	var cell;
+  var cell;
   if (showLabels) {
-		cell = row.selectAll(".cell")
-			.data(function(d, i) { return dataLabels[i]; })
-			.enter().append("g")
-			.attr("class", "cell")
-			.attr("transform", function(d, i) { return "translate(" + i*height + ", 0)"; });
+    cell = row.selectAll(".cell")
+      .data(function(d, i) { return dataLabels[i]; })
+      .enter().append("g")
+      .attr("class", "cell")
+      .attr("transform", function(d, i) { return "translate(" + i*height + ", 0)"; });
 
-		cell.append('rect')
-			.attr("width", x.rangeBand())
-			.attr("height", y.rangeBand()+1);
+    cell.append('rect')
+      .attr("width", x.rangeBand())
+      .attr("height", y.rangeBand()+1);
 
-		cell.append("text")
-	    .attr("dy", ".32em")
-	    .attr("x", x.rangeBand() / 2)
-	    .attr("y", y.rangeBand() / 2)
-	    .attr("text-anchor", "middle")
-			.style("fill", function(d) {
-				for (var i = 0; i < words.length; i++) {
-					if (d.localeCompare(words[i]) == 0) {
-						if (words[i].localeCompare("'s") == 0) {
-							words.splice(i, 1);
-						}
-						return "black";
-					}
-				}
-				return "#C0C0C0";
-			 })
-		  .style("font-weight", function(d) {
-				for (var i = 0; i < words.length; i++) {
-					if (d.localeCompare(words[i]) == 0) {
-						if (words[i].localeCompare("'s") == 0) {
-							words.splice(i, 1);
-						}
-						return "900";
-					}
-				}
-				return "";
-			 })
-	    .text(function(d, i) { return d; });
-	} else {
-		cell = row.selectAll(".cell")
-			.data(function(d, i) { return d; })
-			.enter().append("g")
-			.attr("class", "cell")
-			.attr("transform", function(d, i) { return "translate(" + x(i) + ", 0)"; });
+    cell.append("text")
+      .attr("dy", ".32em")
+      .attr("x", x.rangeBand() / 2)
+      .attr("y", y.rangeBand() / 2)
+      .attr("text-anchor", "middle")
+      .style("fill", function(d) {
+        for (var i = 0; i < words.length; i++) {
+          if (d.localeCompare(words[i]) == 0) {
+            if (words[i].localeCompare("'s") == 0) {
+              words.splice(i, 1);
+            }
+            return "black";
+          }
+        }
+        return "#C0C0C0";
+       })
+      .style("font-weight", function(d) {
+        for (var i = 0; i < words.length; i++) {
+          if (d.localeCompare(words[i]) == 0) {
+            if (words[i].localeCompare("'s") == 0) {
+              words.splice(i, 1);
+            }
+            return "900";
+          }
+        }
+        return "";
+       })
+      .text(function(d, i) { return d; });
+  } else {
+    cell = row.selectAll(".cell")
+      .data(function(d, i) { return d; })
+      .enter().append("g")
+      .attr("class", "cell")
+      .attr("transform", function(d, i) { return "translate(" + x(i) + ", 0)"; });
 
-		cell.append('rect')
-			.attr("width", x.rangeBand())
-			.attr("height", y.rangeBand()+1);
-	}
+    cell.append('rect')
+      .attr("width", x.rangeBand())
+      .attr("height", y.rangeBand()+1);
+  }
 
-	row.selectAll(".cell")
-		.data(function(d, i) { return dataValues[i]; })
-		.style("fill", colorMap);
+  row.selectAll(".cell")
+    .data(function(d, i) { return dataValues[i]; })
+    .style("fill", colorMap);
 
   if (highlightCellOnHover) {
-		cell
-		.on("mouseover", function(d, i) {
-			d3.select(this).style("transform", "scale(1.1,1.1)").style("transform-origin", "25% 25%");
-		})
-		.on("mouseout", function() {
-			d3.select(this).style("transform", "scale(1.0,1.0)").style("transform-origin", "25% 25%");
-		});
+    cell
+    .on("mouseover", function(d, i) {
+      d3.select(this).style("transform", "scale(1.1,1.1)").style("transform-origin", "25% 25%");
+    })
+    .on("mouseout", function() {
+      d3.select(this).style("transform", "scale(1.0,1.0)").style("transform-origin", "25% 25%");
+    });
   }
 }
